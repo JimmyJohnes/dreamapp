98831640e36d1183beab20ba14613cfb1f382221
JimmyJohnes <amsoliman03@outlook.com>
2023-04-22 14:18:04 +0200

    page building and code implementation added a basic css and html code to view a page and handle input added javascript code to send the from data to a server and await the result, then displays the image on screen

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+let form = document.querySelector('form');
+form.addEventListener('submit',async (e) => {
+  e.preventDefault();
+  let formData = new FormData(form);
+  const data = await fetch('http://localhost:3000/dream', {
+    method: 'POST',
+    headers:
+    {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+    {
+      "prompt": formData.get('prompt')
+    })
+  });
+  const result =  await data.json();
+  console.log(result);
+  document.querySelector('#result').innerHTML = `<img src="${result.image}" width = "512" height = "512" alt = "${formData.get('prompt')}"/>`;
+});
